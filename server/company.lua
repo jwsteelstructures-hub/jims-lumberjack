97c1753b4fe4c51c9fb8a7f7f903567815b94315
Jim Jam <j.w.steelstructures@gmail.com>
2026-04-29 23:30:27 -0400

    fix

--- server/company.lua
+++ server/company.lua
@@ -1,60 +1,23 @@
-Company = {
-    id = nil,
-    name = "Jim's Lumber Co.",
-    funds = 0
-}
-
 function Company.Load()
     print("[Lumberjack] Loading company data...")
 
-    exports.oxmysql:execute(
+    local result = exports.oxmysql:executeSync(
         "SELECT * FROM companies LIMIT 1",
-        {},
-        function(result)
-            if result and result[1] then
-                Company.id = result[1].id
-                Company.name = result[1].name
-                Company.funds = result[1].funds
-                print("[Lumberjack] Company loaded:", Company.name, "Funds:", Company.funds)
-            else
-                exports.oxmysql:insert(
-                    "INSERT INTO companies (name, funds) VALUES (?, ?)",
-                    { Company.name, 0 },
-                    function(insertId)
-                        Company.id = insertId
-                        print("[Lumberjack] Created new company:", Company.name)
-                    end
-                )
-            end
-        end
-    )
-end
-
-function Company.AddFunds(amount)
-    Company.funds = Company.funds + amount
-
-    exports.oxmysql:update(
-        "UPDATE companies SET funds = ? WHERE id = ?",
-        { Company.funds, Company.id }
+        {}
     )
 
-    Company.LogTransaction("add", amount)
-end
+    if result and result[1] then
+        Company.id = result[1].id
+        Company.name = result[1].name
+        Company.funds = result[1].funds
+        print("[Lumberjack] Company loaded:", Company.name, "Funds:", Company.funds)
+    else
+        local insertId = exports.oxmysql:insertSync(
+            "INSERT INTO companies (name, funds) VALUES (?, ?)",
+            { Company.name, 0 }
+        )
 
-function Company.RemoveFunds(amount)
-    Company.funds = Company.funds - amount
-
-    exports.oxmysql:update(
-        "UPDATE companies SET funds = ? WHERE id = ?",
-        { Company.funds, Company.id }
-    )
-
-    Company.LogTransaction("remove", amount)
-end
-
-function Company.LogTransaction(type, amount)
-    exports.oxmysql:insert(
-        "INSERT INTO company_transactions (company_id, type, amount) VALUES (?, ?, ?)",
-        { Company.id, type, amount }
-    )
+        Company.id = insertId
+        print("[Lumberjack] Created new company:", Company.name)
+    end
 end
