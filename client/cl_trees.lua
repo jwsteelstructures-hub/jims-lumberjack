c74043f9beaebabb7b21c921f92068df84a373ac
Jim Jam <j.w.steelstructures@gmail.com>
2026-05-08 00:16:07 -0400

    adjustment

--- client/cl_trees.lua
+++ client/cl_trees.lua
@@ -251,11 +251,11 @@ RegisterNetEvent("jims-lumberjack:treeFalling", function(treeId)
     --========================================================--
     --  MODEL-SPECIFIC ALIGNMENT FIX
     --========================================================--
-    local endOffsetX = -0.6   -- shifted left by 1 meter
+    local endOffsetX = -0.7   -- shifted left by 1 meter
     local endOffsetY = -0.8
 
     if endModel == "des_treefall_up15_end" then
-        endOffsetX = -0.6
+        endOffsetX = -0.7
         endOffsetY = -1.2
     end
 
